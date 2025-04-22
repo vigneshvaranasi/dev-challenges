a9882e70f877b9a622965b43972a2440135a86ab
vigneshvaranasi <varanasivsv@gmail.com>
2025-04-22 07:41:52 +0530

    feat: enhance toggle component with sub-selection for Premium plan

--- 04-Animated-Toggles/src/App.jsx
+++ 04-Animated-Toggles/src/App.jsx
@@ -3,79 +3,95 @@ import { AnimatePresence, motion } from 'framer-motion'
 
 function App () {
   const [selected, setSelected] = useState('Free')
+  const [subSelected, setSubSelected] = useState('Monthly')
 
   return (
     <div className='text-md font-semibold'>
-      <div className='md:w-1/3 bg-white relative mx-auto mt-10 p-1 border border-[#eee] rounded-full shadow-sm flex items-center'>
+      <div className='md:w-1/3 bg-white relative mx-auto mt-10 border border-[#eee] rounded-full shadow-sm flex items-center h-14'>
         <motion.div
-          className='absolute top-1 bottom-1 left-0 bg-black rounded-full w-1/2'
+          className='absolute top-0.5 bottom-0.5 left-0.5 bg-black rounded-full w-1/2'
           animate={{ left: selected === 'Free' ? '1%' : '49%' }}
           transition={{ type: 'spring', stiffness: 500, damping: 50 }}
         />
         <p
           onClick={() => setSelected('Free')}
-          className={`z-10 p-2 text-center ${
+          className={`z-10 text-center ${
             selected === 'Free' ? 'text-white' : 'text-black'
-          } cursor-pointer w-1/2`}
+          } cursor-pointer w-1/2 flex items-center justify-center`}
         >
           Free
         </p>
 
-        <AnimatePresence mode='wait'>
-          {selected === 'Free' ? (
-            <motion.div
-              key='free'
-              className='flex flex-col w-1/2 text-center cursor-pointer'
-            >
+        <motion.div
+          onClick={() => {
+            setSelected('Premium')
+            setSubSelected('Monthly')
+          }}
+          className={`flex flex-col w-1/2 text-center cursor-pointer py-2`}
+          transition={{ type: 'spring', stiffness: 700, damping: 30 }}
+        >
+          <AnimatePresence>
+            {selected !== 'Premium' && (
               <motion.p
-                exit={{ opacity: 0 }}
-                onClick={() => setSelected('Premium')}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, scale: 1, y: 0 }}
+                exit={{ opacity: 0, scale: 0.8 }}
+                transition={{ duration: 0.015 }}
               >
                 Premium
               </motion.p>
-              <motion.p
-                exit={{ opacity: 0, scale: 1.2 }}
-                className='font-normal'
-                onClick={() => setSelected('Premium')}
-              >
-                
-                <span>Monthly</span>
-                <span className='mt-2 mx-1 font-bold'>.</span>
-                <span>Annual</span>
-                
-              </motion.p>
-            </motion.div>
-          ) : (
-            <motion.div
-              key='premium'
-              className='flex justify-between items-center relative w-1/2 rounded-full p-1 gap-1'
-            >
+            )}
+          </AnimatePresence>
+          <motion.div
+            animate={{
+              alignItems: 'center',
+              zIndex: selected === 'Premium' ? 100 : 0
+            }}
+            transition={{
+              type: 'spring',
+              stiffness: 600,
+              damping: 30
+            }}
+            className={`font-normal relative p-0 m-0 flex items-center justify-center ${
+              selected === 'Premium' ? 'px-1' : 'px-0'
+            }`}
+          >
+            {selected === 'Premium' && (
               <motion.div
-                initial={{ left: '0%' }}
-                className='absolute top-1 bottom-1 bg-white rounded-full w-1/2'
-                animate={{ left: selected === 'Premium' ? '0%' : '48%' }}
-                transition={{ type: 'spring', stiffness: 500, damping: 50 }}
+                className='absolute top-0.5 bottom-0.5 -left-2 bg-white rounded-full w-1/2'
+                initial={{ left: '1%' }}
+                animate={{ left: subSelected === 'Monthly' ? '1%' : '49%' }}
+                transition={{ type: 'spring', stiffness: 500, damping: 10 }}
               />
-
-              <motion.p
-                onClick={() => setSelected('Premium')}
-                className={`z-10 p-2 text-center ${
-                  selected === 'Premium' ? 'text-black' : 'text-white'
-                } cursor-pointer w-1/2`}
-              >
-                Monthly
-              </motion.p>
-              <p
-                onClick={() => setSelected('Annual')}
-                className={`z-10 p-2 text-center ${
-                  selected === 'Annual' ? 'text-black' : 'text-white'
-                } cursor-pointer w-1/2`}
-              >
-                Annual
-              </p>
-            </motion.div>
-          )}
-        </AnimatePresence>
+            )}
+            <motion.p
+              animate={{
+                scale: selected === 'Premium' ? 1 : 0.98,
+                justifyContent: 'space-between'
+              }}
+              transition={{ type: 'spring', stiffness: 600, damping: 30 }}
+              className={`p-3 m-0 ${
+                selected === 'Premium' ? 'text-black w-1/2 z-200' : 'text-black'
+              }`}
+            >
+              Monthly
+            </motion.p>
+            {selected === 'Free' && <p>.</p>}
+            <motion.p
+              className={`p-0 m-0 ${
+                selected === 'Premium'
+                  ? 'text-white w-1/2 rounded-full'
+                  : 'text-black'
+              }`}
+              animate={{
+                scale: selected === 'Premium' ? 1.1 : 0.98
+              }}
+              transition={{ type: 'spring', stiffness: 600, damping: 30 }}
+            >
+              Annual
+            </motion.p>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   )
